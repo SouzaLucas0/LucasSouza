8297baf2e1807cc77fffa2b94ab1a24b7696cd3a
Lucas Souza <lucas.souza.dev@outlook.com.br>
2024-11-06 22:05:07 -0400

    aula 01

---
+++ portas/components/porta/Porta.jsx
@@ -0,0 +1,18 @@
+import styles from "./Porta.module.css"
+
+export default function Porta (props) {
+
+    return (
+        <div className={styles.area}>
+            <div className={styles.frame}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>
+                        3
+                    </div>
+                </div>
+            </div>
+            <div className={styles.chao}></div>
+        </div>
+    )
+
+}
