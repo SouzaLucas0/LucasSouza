aae923eb0b2dbce8ccfcffbb3091a2672ac66ab7
Lucas Souza <lucas.souza.dev@outlook.com.br>
2024-11-13 11:15:39 -0400

    update

--- portifolio/components/assets/sections/home/SectionHome.jsx
+++ portifolio/components/assets/sections/home/SectionHome.jsx
@@ -1,12 +1,18 @@
 import styles from './home.module.css'
+import Image from 'next/image'
 
-export default function SectionHome (props) {
-   const fotoPerfil = "/images/perfil.png"
+export default function SectionHome () {
 
     return (
         
-        <section id='home' className={styles.home}> 
-            
+        <section id='home' className={styles.home}>
+            <Image 
+                className={styles.home__fotoPerfil}
+                src="/images/perfil.png"
+                width={500}
+                height={10}
+                alt="Picture of the author"
+            />
         </section>
         
     )
