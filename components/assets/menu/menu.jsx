04fc62c7b0c2abff87b6bc317a3478df331a8b92
Lucas Souza <lucas.souza.dev@outlook.com.br>
2024-12-16 22:17:37 -0400

    update

--- components/assets/menu/menu.jsx
+++ components/assets/menu/menu.jsx
@@ -1,31 +1,43 @@
 import logo from '/public/img/logo.png'
 import styles from './menu.module.css'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Menu (props) {
     
     const btnList = props.botao
-
+    const [menuOpen, setOpen] = useState(true)
     return(
          <>
             <Image
                 src={logo}
                 alt="logo do site"
-                className={styles.logo}                
+                className={styles.logo}
             />
             
-            <div className={styles.menu__navBar}>
+            <div className={`${styles.menu__navBar} ${menuOpen ? styles.open : styles.close }`}>
                 {btnList.map((btn, index)=>{
                     return <li key={index}><a href={btn.link}>{btn.nome}</a></li>
                 })}
             </div>
 
+            
             <div className={styles.menu__navBarMobile}>
-                <div className={styles.menu__mobileIcon}>
-                        sadfas
-                    
-                </div>
+                    <button 
+                        className={styles.hamburger}
+                        onClick={openMenu}
+                    >
+                    </button>
             </div>
-        </>   
+        </>
     )
+
+    function openMenu() {
+        if (menuOpen) {
+            setOpen(false)
+        } else {
+            setOpen(true)
+        }
+    }
+
 }
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -24,7 +24,7 @@ const btnList = [
                     logo= {logo}
                     botao= {btnList}
                 />
-            </div>
+            </div>            
         </header>
     )
 }
